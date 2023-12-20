b2b39676d48ceb356c3ce1e528cb9952829af766
Yizhe ZHANG <elecoxy@gmail.com>
2023-12-20 13:13:40 +0800

    Initial Project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="">
+        <p class="title">
+          Hooli.hk
+        </p>
+        <p class="subtitle">
+          Welcome to our official website.
+        </p>
+        <p>
+          Currently under development...
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
